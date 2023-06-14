21a36539c9429e31ee9a1949c9da0daefc2da3b1
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-06-14 23:21:17 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [click, setClick] = useState(0);
+
+  const btnClicked = () => {
+    setClick(click+1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {click} times</p>
+        <button onClick={btnClicked}>Click me</button>
     </div>
   )
 }
